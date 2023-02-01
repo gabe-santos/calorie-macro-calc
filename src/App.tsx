4e52440b83a3c84bfd3cd0f77c1e2a43d0a2ccc5
Gabe <28gabe@gmail.com>
2023-02-01 05:22:41 -0800

    first commit from local

--- src/App.tsx
+++ src/App.tsx
@@ -6,188 +6,193 @@ import { Slider } from './Slider';
 import { Tooltip } from './Tooltip';
 
 export const App = () => {
-  const dietOptions = ['Standard'];
-  const unitOptions = ['Imperial', 'Metric'];
-  const activityOptions = [
-    'Sedentary',
-    'Lightly Active',
-    'Moderately Active',
-    'Very Active',
-    'Extremely Active',
-  ];
-  const goalOptions = [
-    'Lose Weight',
-    'Slowly Lose Weight',
-    'Maintain Weight',
-    'Slowly Gain Weight',
-    'Gain Weight',
-  ];
+	const dietOptions = ['Standard'];
+	const unitOptions = ['Imperial', 'Metric'];
+	const activityOptions = [
+		'Sedentary',
+		'Lightly Active',
+		'Moderately Active',
+		'Very Active',
+		'Extremely Active',
+	];
+	const goalOptions = [
+		'Lose Weight',
+		'Slowly Lose Weight',
+		'Maintain Weight',
+		'Slowly Gain Weight',
+		'Gain Weight',
+	];
 
-  const [age, setAge] = useState(18);
-  const [weight, setWeight] = useState(120);
-  const [height, setHeight] = useState(60);
-  const [sex, setSex] = useState('male');
-  const [activityLvl, setActivityLvl] = useState('Sedentary');
-  const [goal, setGoal] = useState('Maintain Weight');
-  const [proteinVal, setProteinVal] = useState(1.0);
-  const [carbPercent, setCarbPercent] = useState(50);
-  const [bmr, setBmr] = useState(0);
+	const [age, setAge] = useState(18);
+	const [weight, setWeight] = useState(120);
+	const [height, setHeight] = useState(60);
+	const [sex, setSex] = useState('male');
+	const [activityLvl, setActivityLvl] = useState('Sedentary');
+	const [goal, setGoal] = useState('Maintain Weight');
+	const [proteinVal, setProteinVal] = useState(1.0);
+	const [carbPercent, setCarbPercent] = useState(50);
+	const [bmr, setBmr] = useState(0);
 
-  const handleAgeChange = (e: {
-    target: { value: SetStateAction<number> };
-  }) => {
-    setAge(e.target.value);
-  };
+	const handleAgeChange = (e: {
+		target: { value: SetStateAction<number> };
+	}) => {
+		setAge(e.target.value);
+	};
 
-  const handleWeightChange = (e: {
-    target: { value: SetStateAction<number> };
-  }) => {
-    setWeight(e.target.value);
-  };
+	const handleWeightChange = (e: {
+		target: { value: SetStateAction<number> };
+	}) => {
+		setWeight(e.target.value);
+	};
 
-  const handleHeightChange = (e: {
-    target: { value: SetStateAction<number> };
-  }) => {
-    setHeight(e.target.value);
-  };
+	const handleHeightChange = (e: {
+		target: { value: SetStateAction<number> };
+	}) => {
+		setHeight(e.target.value);
+	};
 
-  const handleSexChange = (e: {
-    target: { value: SetStateAction<number> };
-  }) => {
-    console.log(e.target);
-    setSex(e.target.value);
-  };
+	const handleSexChange = (e: {
+		target: { value: SetStateAction<number> };
+	}) => {
+		console.log(e.target);
+		setSex(e.target.value);
+	};
 
-  const handleActivityChange = (e: {
-    target: { value: SetStateAction<number> };
-  }) => {
-    console.log(e.target);
-    setActivityLvl(e.target.value);
-  };
+	const handleActivityChange = (e: {
+		target: { value: SetStateAction<number> };
+	}) => {
+		console.log(e.target);
+		setActivityLvl(e.target.value);
+	};
 
-  const handleGoalChange = (e: {
-    target: { value: SetStateAction<number> };
-  }) => {
-    console.log(e.target);
-    setGoal(e.target.value);
-  };
+	const handleGoalChange = (e: {
+		target: { value: SetStateAction<number> };
+	}) => {
+		console.log(e.target);
+		setGoal(e.target.value);
+	};
 
-  const handleProteinChange = (e: {
-    target: { value: SetStateAction<number> };
-  }) => {
-    setProteinVal(e.target.value);
-  };
+	const handleProteinChange = (e: {
+		target: { value: SetStateAction<number> };
+	}) => {
+		setProteinVal(e.target.value);
+	};
 
-  const handleCarbSplitChange = (e: {
-    target: { value: SetStateAction<number> };
-  }) => {
-    setCarbPercent(e.target.value);
-  };
+	const handleCarbSplitChange = (e: {
+		target: { value: SetStateAction<number> };
+	}) => {
+		setCarbPercent(e.target.value);
+	};
 
-  useEffect(() => {
-    setBmr(() => calculateBMR());
-  });
+	useEffect(() => {
+		setBmr(() => calculateBMR());
+	});
 
-  const calculateBMR = () => {
-    let mass = weight * 0.453592;
-    let heightCM = height * 2.54;
-    return mifflinStJeor(mass, heightCM);
-  };
+	const calculateBMR = () => {
+		let mass = weight * 0.453592;
+		let heightCM = height * 2.54;
+		return mifflinStJeor(mass, heightCM);
+	};
 
-  const mifflinStJeor = (mass, heightCM) => {
-    let s = sex == 'male' ? 5 : -161;
-    return Math.floor(10 * mass + 6.25 * heightCM - 5.0 * age + s);
-  };
+	const mifflinStJeor = (mass, heightCM) => {
+		let s = sex == 'male' ? 5 : -161;
+		return Math.floor(10 * mass + 6.25 * heightCM - 5.0 * age + s);
+	};
 
-  return (
-    <div className="App">
-      <h1 className="text-4xl">Calorie and Macro Calculator</h1>
-      <div className="container p-5 mx-auto">
-        <Select label={'Diet'} options={dietOptions} />
-        <Select label={'Units'} options={unitOptions} />
-        <NumInput label={'Age'} value={age} onChange={handleAgeChange} />
-        <NumInput
-          label={'Weight'}
-          value={weight}
-          onChange={handleWeightChange}
-        />
-        <NumInput
-          label={'Height'}
-          value={height}
-          onChange={handleHeightChange}
-        />
+	return (
+		<div className='App'>
+			<h1 className='text-4xl'>Calorie and Macro Calculator</h1>
+			<div className='container p-5 mx-auto'>
+				<Select label={'Diet'} options={dietOptions} />
+				<Select label={'Units'} options={unitOptions} />
+				<NumInput
+					label={'Age'}
+					value={age}
+					onChange={handleAgeChange}
+				/>
+				<NumInput
+					label={'Weight'}
+					value={weight}
+					onChange={handleWeightChange}
+				/>
+				<NumInput
+					label={'Height'}
+					value={height}
+					onChange={handleHeightChange}
+				/>
 
-        <form className="container flex-col">
-          <label className="label">Sex</label>
-          <div className="btn-group flex">
-            <input
-              type="radio"
-              name="sex"
-              className="btn"
-              data-title="Male"
-              value="male"
-              onChange={handleSexChange}
-            />
-            <input
-              type="radio"
-              name="sex"
-              className="btn"
-              data-title="Female"
-              value="female"
-              onChange={handleSexChange}
-            />
-          </div>
-        </form>
+				<form className='container flex-col'>
+					<label className='label'>Sex</label>
+					<div className='btn-group flex'>
+						<input
+							type='radio'
+							name='sex'
+							className='btn'
+							data-title='Male'
+							value='male'
+							onChange={handleSexChange}
+						/>
+						<input
+							type='radio'
+							name='sex'
+							className='btn'
+							data-title='Female'
+							value='female'
+							onChange={handleSexChange}
+						/>
+					</div>
+				</form>
 
-        <div className="flex">
-          <Select
-            label={'Activity Level'}
-            options={activityOptions}
-            value={activityLvl}
-            onChange={handleActivityChange}
-          />
-          {/* <Tooltip content={'level of activity'} /> */}
-        </div>
-        <Select
-          label={'Goal'}
-          options={goalOptions}
-          value={goal}
-          onChange={handleGoalChange}
-        />
+				<div className='flex'>
+					<Select
+						label={'Activity Level'}
+						options={activityOptions}
+						value={activityLvl}
+						onChange={handleActivityChange}
+					/>
+					{/* <Tooltip content={'level of activity'} /> */}
+				</div>
+				<Select
+					label={'Goal'}
+					options={goalOptions}
+					value={goal}
+					onChange={handleGoalChange}
+				/>
 
-        <Slider
-          label={'Daily Protein'}
-          min={0.82}
-          max={1.5}
-          step={0.01}
-          value={proteinVal}
-          unit={'g'}
-          onChange={handleProteinChange}
-        />
-        <Slider
-          label={'Daily Carb/Protein Split'}
-          min={0}
-          max={100}
-          step={1}
-          value={carbPercent}
-          unit={`/${100 - carbPercent}`}
-          onChange={handleCarbSplitChange}
-        />
-      </div>
-      <div className="container">
-        <h1>Test Display</h1>
-        <div>Age: {age}</div>
-        <div>Weight: {weight}</div>
-        <div>Height: {height}</div>
-        <div>Sex: {sex}</div>
-        <div>Activity Level: {activityLvl}</div>
-        <div>Goal: {goal}</div>
-        <div>Daily Protein: {proteinVal}</div>
-        <div>
-          Daily Carb/Protein Split: {`${carbPercent}/${100 - carbPercent}`}
-        </div>
-        <div>BMR: {bmr}</div>
-      </div>
-    </div>
-  );
+				<Slider
+					label={'Daily Protein'}
+					min={0.82}
+					max={1.5}
+					step={0.01}
+					value={proteinVal}
+					unit={'g'}
+					onChange={handleProteinChange}
+				/>
+				<Slider
+					label={'Daily Carb/Protein Split'}
+					min={0}
+					max={100}
+					step={1}
+					value={carbPercent}
+					unit={`/${100 - carbPercent}`}
+					onChange={handleCarbSplitChange}
+				/>
+			</div>
+			<div className='container'>
+				<h1>Test Display</h1>
+				<div>Age: {age}</div>
+				<div>Weight: {weight}</div>
+				<div>Height: {height}</div>
+				<div>Sex: {sex}</div>
+				<div>Activity Level: {activityLvl}</div>
+				<div>Goal: {goal}</div>
+				<div>Daily Protein: {proteinVal}</div>
+				<div>
+					Daily Carb/Protein Split:{' '}
+					{`${carbPercent}/${100 - carbPercent}`}
+				</div>
+				<div>BMR: {bmr}</div>
+			</div>
+		</div>
+	);
 };
--- src/Select.tsx
+++ src/Select.tsx
@@ -1,17 +1,17 @@
 interface SelectProps {
-  label: string;
-  options: string[];
+	label: string;
+	options: string[];
 }
 
 export const Select = ({ label, options }: SelectProps) => {
-  return (
-    <div className="form-control">
-      <label className="label text-lg">{label}</label>
-      <select className="select select-bordered max-w-xs">
-        {options.map((item) => (
-          <option>{item}</option>
-        ))}
-      </select>
-    </div>
-  );
+	return (
+		<div className='form-control'>
+			<label className='label text-lg'>{label}</label>
+			<select className='select select-bordered max-w-xs'>
+				{options.map(item => (
+					<option key={item}>{item}</option>
+				))}
+			</select>
+		</div>
+	);
 };
