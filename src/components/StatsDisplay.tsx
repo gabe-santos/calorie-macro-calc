afd6abc4aa680ea97eea510bb04ad8cd1078fb97
Gabe <28gabe@gmail.com>
2023-05-02 21:12:05 -0700

    removes colors

--- src/components/StatsDisplay.tsx
+++ src/components/StatsDisplay.tsx
@@ -12,24 +12,24 @@ export const StatsDisplay = ({
 	fat,
 }: StatsDisplayProps) => {
 	return (
-		<div className='stats flex flex-col shadow-lg stats-vertical bg-blue-400'>
+		<div className='stats flex flex-col shadow-md stats-vertical bg-zinc-50'>
 			<div className='stat place-items-center border-none'>
 				<div className='stat-value'>{calories}</div>
 				<div className='stat-desc'>calories</div>
 			</div>
 
-			<div className='stats flex rounded-t-none border-none'>
-				<div className='stat place-items-center bg-rose-500'>
+			<div className='stats flex rounded-t-none border-none bg-zinc-200'>
+				<div className='stat place-items-center '>
 					<div className='stat-value text-3xl'>{protein}</div>
 					<div className='stat-desc'>g of protein</div>
 				</div>
 
-				<div className='stat place-items-center bg-emerald-500'>
+				<div className='stat place-items-center border-zinc-300'>
 					<div className='stat-value text-3xl'>{carbs}</div>
 					<div className='stat-desc '>g of carbs</div>
 				</div>
 
-				<div className='stat place-items-center bg-amber-500'>
+				<div className='stat place-items-center border-zinc-300'>
 					<div className='stat-value text-3xl'>{fat}</div>
 					<div className='stat-desc'>g of fat</div>
 				</div>
